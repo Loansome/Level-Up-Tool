ebd30fd6b39fd9d3e44243ea0972a41e9afd6a29
Loansome <gabesalomone@gmail.com>
2022-11-18 01:28:37 -0600

    Curve/Bonuses Working
    Levels can now be controlled by variables and be reflected in the curve, which is now free to be adjusted for the resulting curve. Also found a bug that happens when the user undo's, but can be fixed by undoing or redoing again to update the inspector back.

--- Assets/Scripts/LevelSystem.cs
+++ Assets/Scripts/LevelSystem.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 
 public class LevelSystem : MonoBehaviour
 {
+    [Header("References")]
     public Player playerStats;
     public Slider experienceBar;
     public Text experienceText;
     public Text levelText;
 
-    public AnimationCurve experienceLevelCurve;
-    public int totalLevels;
-    public int totalExperience;
+    [Header("Level Data")]
+    public Vector2Int minMaxLevel = new Vector2Int(0, 10); // X value is minimum level, Y value is maximum level
+    public Vector2Int minMaxExperience = new Vector2Int(0, 100); // X value is minimum experience, Y value is maximum experience
+    public AnimationCurve experienceCurve;
     public int currentLevel;
     public int currentExperience;
     public int previousLevel;
@@ -22,33 +24,35 @@ public class LevelSystem : MonoBehaviour
 
 	public void OnValidate()
 	{
-        if (levelUpBonus.Count != totalLevels)
-        {
-            for (int i = 0; i < totalLevels; i++)
-            {
-                levelUpBonus.Add(new LevelBonus());
-                levelUpBonus[i] = new("Level " + (i + 1), 10f, 5f, 2f, 1f);
-            }
+        if (experienceCurve.length < 2)
+		{
+            Debug.Log("No values to pull from. Resetting curve.");
+            experienceCurve.keys = new Keyframe[2];
+            experienceCurve.keys[0] = new Keyframe(0, 0);
+            experienceCurve.keys[1] = new Keyframe(10, 100);
+            return;
         }
-        totalExperience = (int)experienceLevelCurve[experienceLevelCurve.length - 1].value;
-        int curveSize = (int)experienceLevelCurve[experienceLevelCurve.length - 1].time;
+        Keyframe[] curveKeys = experienceCurve.keys;
+        curveKeys[0].time = minMaxLevel.x;
+        curveKeys[0].value = minMaxExperience.x;
+        curveKeys[experienceCurve.length - 1].time = minMaxLevel.y;
+        curveKeys[experienceCurve.length - 1].value = minMaxExperience.y;
+        experienceCurve.keys = curveKeys;
 
-        if (totalLevels < curveSize)
+        if (minMaxLevel.y > levelUpBonus.Count)
         {
-            for (int i = totalLevels; i < curveSize; i++)
+            for (int i = levelUpBonus.Count; i < minMaxLevel.y; i++)
             {
                 levelUpBonus.Add(new LevelBonus());
                 levelUpBonus[i] = new("Level " + (i + 1), 10f, 5f, 2f, 1f);
             }
         }
-        else if (totalLevels > curveSize)
+        else if (minMaxLevel.y < levelUpBonus.Count)
         {
-            for (int i = curveSize; i < totalLevels; i++)
+            for (int i = minMaxLevel.y; i < levelUpBonus.Count; i++)
             {
                 levelUpBonus.RemoveAt(levelUpBonus.Count - 1);
             }
         }
-
-        totalLevels = curveSize;
 	}
 }
